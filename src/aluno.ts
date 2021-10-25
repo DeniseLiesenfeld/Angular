7abacfb92ba8933f95ec19122f9df4a1149d8dab
DeniseLiesenfeld <denise.liesenfeld@gmail.com>
2021-10-25 19:50:50 -0300

    ok-trabalho

---
+++ src/aluno.ts
@@ -0,0 +1,32 @@
+export const dados = [ {
+
+    nome :"Denise Liesenfeld",
+    ru:'2150655',
+    curso:'Análise e desenvolvimento de sistemas',
+    dataNascimento:'21/07/1991'
+    
+},{
+    nome :"Ana clara",
+    ru:'5858745',
+    curso:'Análise e desenvolvimento de sistemas',
+    dataNascimento:'22/10/1995'
+},{
+    nome :"Fabio",
+    ru:'0058857',
+    curso:'Fármacia',
+    dataNascimento:'10/10/1998'
+},{
+    nome :"Paulo Santos",
+    ru:'1458752',
+    curso:'Veterinária',
+    dataNascimento:'15/06/1990'
+},{
+    nome :"Julia Zoura",
+    ru:'1985478',
+    curso:'Psicologia',
+    dataNascimento:'22/10/1995'
+}
+
+       
+        
+]
